be533783a67e9c09eb3b27ef754fe1f4d6daeebc
407-18-Dueannakhon <54377@yupparaj.ac.th>
2026-08-21 12:08:33 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -99,14 +99,6 @@ ans2 = st.text_input(
     "ข้อ 2: Cats love to eat `f _ s h`. 🐟",
     value=st.session_state.ans2_val,
 )
-
-# อัปเดตค่าล่าสุดเข้าตัวแปร
-st.session_state.ans1_val = ans1
-st.session_state.ans2_val = ans2
-st.session_state.ans3_val = ans3
-st.session_state.ans4_val = ans4
-
-# ✏️ [พื้นที่สำหรับนักเรียน]: เพิ่มข้อ 3, 4 ตรงนี้
 ans3 = st.text_input(
     "ข้อ 3: A `ch _ _ry ` on top.🍒 ",
     value=st.session_state.ans3_val,
@@ -115,6 +107,13 @@ ans4 = st.text_input(
     "ข้อ 4: Life gives you `le_ _ ns`,make lemonade.🍋 ",
     value=st.session_state.ans4_val,
 )    
+# อัปเดตค่าล่าสุดเข้าตัวแปร
+st.session_state.ans1_val = ans1
+st.session_state.ans2_val = ans2
+st.session_state.ans3_val = ans3
+st.session_state.ans4_val = ans4
+
+# ✏️ [พื้นที่สำหรับนักเรียน]: เพิ่มข้อ 3, 4 ตรงนี้
 # 4. ปุ่มส่งคำตอบ
 if "start" in st.session_state and not st.session_state.get("is_ended", False):
     if st.button("📥 ส่งคำตอบ"):
